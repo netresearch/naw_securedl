94a7f98109240c603efa0a5ee6107ccee2c62e94
Sebastian Mendel <info@sebastianmendel.de>
2013-05-28 10:12:56 +0200

    do not unserialize $_EXTCONF if not required (performance)
    In TYPO3_MODE === 'FE' there is no need for unserialized $_EXTCONF - so we unseraliaze it only if TYPO3_MODE === 'BE'.

--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,15 @@
 <?php
 if (!defined ('TYPO3_MODE'))     die ('Access denied.');
 
-$_EXTCONF = unserialize($_EXTCONF);
-
-if (TYPO3_MODE == 'BE' AND $_EXTCONF['log'])	{
-	t3lib_extMgm::addModule('tools','txnawsecuredlM1','',t3lib_extMgm::extPath($_EXTKEY).'modLog/');
+if (TYPO3_MODE == 'BE')	{
+    $_EXTCONF = unserialize($_EXTCONF);
+    if ($_EXTCONF['log'])	{
+	    t3lib_extMgm::addModule(
+	        'tools', 'txnawsecuredlM1', '',
+	        t3lib_extMgm::extPath($_EXTKEY) . 'modLog/'
+	    );
+	    unset ($_EXTCONF);
+    }
 }
-
-unset ($_EXTCONF);
 
 ?>
